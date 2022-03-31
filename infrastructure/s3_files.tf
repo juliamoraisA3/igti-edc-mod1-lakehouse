23d37d4d8f0fc354e677d63ba02f2ea8f5f2f94e
Júlia Morais <julia.morais@a3data.com.br>
2022-03-30 23:11:24 -0300

    fix aws_s3_object name and labda function name

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket_object" "delta_insert" {
+resource "aws_s3_object" "delta_insert" {
   bucket = aws_s3_bucket.dl.id
   key = "emr-code/pyspark/01_delta_spark_insert.py"
   acl = "private"
   source = "../etl/01_delta_spark_insert.py"
 }
 
-resource "aws_s3_bucket_object" "delta_upsert" {
+resource "aws_s3_object" "delta_upsert" {
   bucket = aws_s3_bucket.dl.id
   key = "emr-code/pyspark/02_delta_spark_upsert.py"
   acl = "private"
