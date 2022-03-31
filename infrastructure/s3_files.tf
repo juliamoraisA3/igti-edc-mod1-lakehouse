9f9209f25b5b76b8e815ef47b56b46706b1de7b1
Júlia Morais <julia.morais@a3data.com.br>
2022-03-30 23:03:13 -0300

    remove etag and add file upsert

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -3,7 +3,6 @@ resource "aws_s3_bucket_object" "delta_insert" {
   key = "emr-code/pyspark/01_delta_spark_insert.py"
   acl = "private"
   source = "../etl/01_delta_spark_insert.py"
-  etag = filemd5("../etl/01_delta_spark_insert.py")
 }
 
 resource "aws_s3_bucket_object" "delta_upsert" {
@@ -11,5 +10,4 @@ resource "aws_s3_bucket_object" "delta_upsert" {
   key = "emr-code/pyspark/02_delta_spark_upsert.py"
   acl = "private"
   source = "../etl/02_delta_spark_upsert.py"
-  etag = filemd5("../etl/02_delta_spark_upsert.py")
 }
